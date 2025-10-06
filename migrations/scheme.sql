e06c91153463ea07940fd0e016b97fafeeab510a
Taji Yorutsuki <55313538+jimuelportugal@users.noreply.github.com>
2025-10-06 20:38:03 +0800

    Update scheme.sql
    Modified Schema

--- migrations/scheme.sql
+++ migrations/scheme.sql
@@ -1,8 +1,15 @@
+-- Added two new colum: borrowed_book and borrow_status
+-- burrow_book: store the title of a book
+-- borrow_status: if the user returned the book(Default "TRUE" if no book is borrowed)
+
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(50) NOT NULL DEFAULT 'user',
     refresh_token VARCHAR(255),
+    borrowed_book VARCHAR(255) DEFAULT 'None',  
+    borrow_status BOOLEAN DEFAULT TRUE,         
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-)
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
