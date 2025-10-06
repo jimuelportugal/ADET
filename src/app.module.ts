cdc1156947f3561b9316248e4ed0ed84aefed0db
Taji Yorutsuki <55313538+jimuelportugal@users.noreply.github.com>
2025-10-06 20:41:45 +0800

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
+import { AppController } from './app.controller';
 
 @Module ({
     imports: [DatabaseModule, UsersModule, AuthModule],
+    controllers: [AppController],
 })
 export class AppModule {}
