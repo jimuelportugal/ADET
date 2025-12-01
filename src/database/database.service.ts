cce4338556deb28b84ea7e66a258b30c2abc71a2
Taji Yorutsuki <55313538+jimuelportugal@users.noreply.github.com>
2025-12-01 08:11:14 +0800

    Update database connection settings in DatabaseService

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -10,10 +10,10 @@ export class DatabaseService implements OnModuleInit, OnModuleDestroy {
 
     async onModuleInit() {
         this.pool = mysql.createPool({
-            host: process.env.DB_HOST ||'',
-            port: +(process.env.DB_PORT || ),
-            user: process.env.DB_USER || '',
-            password: process.env.DB_PASSWORD || '',
+            host: process.env.DB_HOST ||'mysql-1586fd5a-gbox-ed0a.d.aivencloud.com',
+            port: +(process.env.DB_PORT || 23418),
+            user: process.env.DB_USER || 'avnadmin',
+            password: process.env.DB_PASSWORD || 'AVNS_WXC_aTlVeyoAIIAd0S',
             database: process.env.DB_NAME || 'defaultdb',
             waitForConnections: true,
             connectionLimit: 10,
@@ -33,4 +33,5 @@ export class DatabaseService implements OnModuleInit, OnModuleDestroy {
     getPool(){
         return this.pool;
     }
+
 }
