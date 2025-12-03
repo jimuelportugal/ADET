ea5712ff9fc4d6556670679fb91277b1ccf50826
Taji Yorutsuki <55313538+jimuelportugal@users.noreply.github.com>
2025-12-04 00:08:36 +0800

    Fix formatting and ensure proper imports in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { AppController } from './app.controller';
-import { BooksModule } from './books/books.module'; // Import the new modul
+import { BooksModule } from './books/books.module';
 
 @Module ({
-    imports: [DatabaseModule, UsersModule, AuthModule, BooksModule], // Add BooksModule
+    imports: [DatabaseModule, UsersModule, AuthModule, BooksModule],
     controllers: [AppController],
 })
 export class AppModule {}
