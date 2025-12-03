48d3c061b409e9cc6b5b0b7313f9d9526d074151
Taji Yorutsuki <55313538+jimuelportugal@users.noreply.github.com>
2025-12-04 07:54:05 +0800

    Refactor users table and add books table
    Removed borrowed_book and borrow_status columns from users table. Added books table to manage book details and borrowing status.

--- migrations/scheme.sql
+++ migrations/scheme.sql
@@ -1,15 +1,9 @@
--- Added two new colum: borrowed_book and borrow_status
--- burrow_book: store the title of a book
--- borrow_status: if the user returned the book(Default "TRUE" if no book is borrowed)
-
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(50) NOT NULL DEFAULT 'user',
     refresh_token VARCHAR(255),
-    borrowed_book VARCHAR(255) DEFAULT 'None',  
-    borrow_status BOOLEAN DEFAULT TRUE,         
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
@@ -21,6 +15,17 @@ CREATE TABLE positions (
     position_name VARCHAR(300) NOT NULL UNIQUE,
     id INT NOT NULL,
     FOREIGN KEY (id) REFERENCES users(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL UNIQUE,
+    image_link VARCHAR(512),
+    borrower_id INT NULL,
+    status ENUM('available', 'borrowed') NOT NULL DEFAULT 'available',
+    FOREIGN KEY (borrower_id) REFERENCES users(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
