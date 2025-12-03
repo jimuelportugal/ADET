ec9bcc644eb29b59618c96cfd3c9df9809653339
Taji Yorutsuki <55313538+jimuelportugal@users.noreply.github.com>
2025-12-04 00:08:17 +0800

    Make BooksModule a global module

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
 import { DatabaseModule } from '../database/database.module';
