c8f2cb7380d1fa055528e26ac02ac0d9b2343929
Taji Yorutsuki <55313538+jimuelportugal@users.noreply.github.com>
2025-12-03 22:28:21 +0800

    Create BooksModule with service and controller

---
+++ src/books/books.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { BooksService } from './books.service';
+import { BooksController } from './books.controller';
+import { DatabaseModule } from '../database/database.module';
+import { PassportModule } from '@nestjs/passport';
+
+@Module({
+    imports: [DatabaseModule, PassportModule],
+    controllers: [BooksController],
+    providers: [BooksService],
+})
+export class BooksModule {}
