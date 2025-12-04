7c626fd3c403ff17e8a0e56a00738635a4735633
Taji Yorutsuki <55313538+jimuelportugal@users.noreply.github.com>
2025-12-04 08:19:24 +0800

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,57 +1,59 @@
-// Added a GET endpoint for the two new colum added: borrowed_book and borrow_status
 import { Controller, Get, Post, Put, Delete, Body, Param, UseGuards, Request } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { JwtAuthGuard } from "../auth/jwt-auth.guard";
+import { RolesGuard } from "../auth/roles.guard";
+import { Roles } from "../auth/roles.decorator";
 
 @Controller('users')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class UsersController {
-    constructor(private usersService: UsersService) { }
+    constructor(private usersService) { }
 
-    @UseGuards(JwtAuthGuard)
     @Get()
+    @Roles('admin')
     async getAll() {
         return this.usersService.getAll();
     }
 
     @Post()
-    async create(@Body() body: { username: string, password: string }) {
+    @Roles('admin')
+    async create(@Body() body) {
         return this.usersService.createUser(body.username, body.password);
     }
 
-    @UseGuards(JwtAuthGuard)
     @Get(':id')
-    async getOne(@Param('id') id: string) {
+    @Roles('admin')
+    async getOne(@Param('id') id) {
         return this.usersService.findById(+id);
     }
    
-    @UseGuards(JwtAuthGuard)
     @Put(':id')
+    @Roles('admin')
     async update(
-        @Param('id') id: string,
-        @Body() body: { username?: string; password?: string; role?: string }
+        @Param('id') id,
+        @Body() body
     ) {
         return this.usersService.updateUser(+id, body);
     }
 
-    @UseGuards(JwtAuthGuard)
     @Delete(':id')
-    async remove(@Param('id') id: string) {
+    @Roles('admin')
+    async remove(@Param('id') id) {
         return this.usersService.deleteUser(+id);
     }
-
+    
     @Post('borrow/:id')
-    async borrowBook(@Param('id') id: string, @Body('book') book: string) {
+    async borrowBook(@Param('id') id, @Body('book') book) {
         return this.usersService.borrowBook(Number(id), book);
     }
 
     @Post('return/:id')
-    async returnBook(@Param('id') id: string) {
+    async returnBook(@Param('id') id) {
         return this.usersService.returnBook(Number(id));
     }
 
     @Get('status/:id')
-    async borrowStatus(@Param('id') id: string) {
+    async borrowStatus(@Param('id') id) {
         return this.usersService.getUserBorrowStatus(Number(id));
     }
-
 }
