2572baefdf58f068bd3f63448e2c113799c29d36
Taji Yorutsuki <55313538+jimuelportugal@users.noreply.github.com>
2025-12-04 08:17:09 +0800

    Add PositionsModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { AppController } from './app.controller';
 import { BooksModule } from './books/books.module';
+import { PositionsModule } from './positions/positions.module';
 
 @Module ({
-    imports: [DatabaseModule, UsersModule, AuthModule, BooksModule],
+    imports: [DatabaseModule, UsersModule, AuthModule, BooksModule, PositionsModule],
     controllers: [AppController],
 })
 export class AppModule {}
